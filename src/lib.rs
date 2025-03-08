9bae6d0bde7e939aed3c614950c17298bec09681
Ricardo Kullock <ricardokullock@gmail.com>
2025-03-08 17:48:40 +0100

    refactor: Simplify mutex handling and improve command setup in Aichat plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,60 @@
-use nvim_oxi::{Dictionary, Function, Object, Result};
-use nvim_oxi::api::types::LogLevel;
-use nvim_oxi::api;
+use nvim_oxi::{
+    api::{
+        self,
+        opts::CreateCommandOpts,
+        types::{CommandArgs, CommandNArgs},
+    },
+    string, Dictionary, Function, Object, Result,
+};
 
 mod config;
-mod ui;
 mod job_runner;
+mod ui;
+
+fn aichat(args: CommandArgs) -> Result<()> {
+    let line1 = args.line1;
+    let line2 = args.line2;
+    let buffer = api::get_current_buf();
+    let ft = buffer
+        .get_name()?
+        .extension()
+        .map(|x| x.to_string_lossy().to_string())
+        .unwrap_or("".into());
+    let line: Result<nvim_oxi::String> = buffer
+        .get_lines(line1 - 1..line2, true)?
+        .reduce(|acc: nvim_oxi::String, e: nvim_oxi::String| string!("{}\n{}", acc, e))
+        .ok_or(api::Error::Other("No lines found".into()).into());
+    let code = string!("```{}\n{}```", ft, line?.to_string_lossy());
+
+    let _ = api::notify(
+        code.to_str()
+            .map_err(|_| api::Error::Other("Could not convert to str".into()))?,
+        api::types::LogLevel::Info,
+        &Default::default(),
+    );
+    Ok(())
+}
 
 #[nvim_oxi::plugin]
 fn aichat_nvim() -> Result<Dictionary> {
-    let show_config_menu = Function::from_fn(|()| {
-        let _ = config::show_config_menu();
-    });
+    let show_config_menu: Function<(), Result<()>> =
+        Function::from_fn(|()| -> Result<()> { config::show_config_menu() });
 
-    let show_current_config = Function::from_fn(|()| {
-        let _ = config::show_current_config();
-    });
+    let show_current_config: Function<(), Result<()>> =
+        Function::from_fn(|()| -> Result<()> { config::show_current_config() });
+
+    let _ = api::create_user_command(
+        "AichatRs",
+        aichat,
+        &CreateCommandOpts::builder()
+            .range(api::types::CommandRange::WholeFile)
+            .nargs(CommandNArgs::Zero)
+            .desc("Run Aichat command")
+            .build(),
+    )?;
 
     Ok(Dictionary::from_iter([
         ("config_menu", Object::from(show_config_menu)),
         ("current_config", Object::from(show_current_config)),
-        ("run_aichat", Object::from(Function::from_fn(|input: String| {
-            let config = match config::get_current_config() {
-                Ok(config) => config,
-                Err(e) => {
-                    api::notify(
-                        &format!("Failed to get config: {}", e),
-                        LogLevel::Error,
-                        &Default::default(),
-                    ).ok();
-                    return Err(e);
-                }
-            };
-            
-            match job_runner::run_aichat_command(&config, &input) {
-                Ok(output) => Ok(output),
-                Err(e) => {
-                    api::notify(
-                        &format!("Failed to run aichat: {}", e),
-                        LogLevel::Error,
-                        &Default::default(),
-                    ).ok();
-                    Err(e)
-                }
-            }
-        }))),
     ]))
 }
